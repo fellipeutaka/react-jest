ca3f25dd553a53591da777e6f601ea1b0877ac29
Fellipe Utaka <fellipeutaka@gmail.com>
2022-04-05 02:19:04 -0300

    Fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import List from "./components/List";
 
 export default function App() {
   return <List initialItems={["Breakfast", "Gym", "Lunch"]} />;
-=======
-export default function App() {
-  return <h1 className="test">Hello Jest with React</h1>;
->>>>>>> 9d005b9141c054d0391e9f3a72844453c325b4c9
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
->>>>>>> 9d005b9141c054d0391e9f3a72844453c325b4c9
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
-  document.getElementById('root')
-)
-=======
   document.getElementById("root")
 );
->>>>>>> 9d005b9141c054d0391e9f3a72844453c325b4c9
